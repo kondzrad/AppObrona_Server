b68e45b43ebc645225abf0fdb9b7ad31338b864e
Konrad <kywka12@gmail.com>
2020-08-30 23:50:24 +0200

    close #13 Logic for reading all employees rev2

--- src/main/java/pl/kawka/appobronaserver/model/Employee.java
+++ src/main/java/pl/kawka/appobronaserver/model/Employee.java
@@ -22,9 +22,9 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "first_name")
-    private String first_name;
+    private String firstName;
     @Column(name = "last_name")
-    private String last_name;
+    private String lastName;
     @Column(name = "status")
     private String status;
     @Column(name = "login")
@@ -40,20 +40,20 @@ public class Employee {
         this.id = id;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getStatus() {
@@ -84,8 +84,8 @@ public class Employee {
     public String toString() {
         return "Employee{" +
                 "id=" + id +
-                ", first_name='" + first_name + '\'' +
-                ", last_name='" + last_name + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 ", status='" + status + '\'' +
                 ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
